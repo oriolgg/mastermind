4ee4bb72f24ef2d5f0f9812ac336df56ca9022a9
Oriol Garcia <oriol.garcia@atresmediahf.com>
2018-03-07 01:22:44 +0100

    Adds gameToJson function and implements new_game as a POST endpoint

--- mastermind/urls.py
+++ mastermind/urls.py
@@ -5,6 +5,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('game', views.new_game, name='new_game'),
+    path('game', views.new_game),
     path('game/<int:game_id>', views.game, name='get_game'),
 ]
--- mastermind/views.py
+++ mastermind/views.py
@@ -8,17 +8,36 @@ from .mastermind import Mastermind
 
 import json
 
-def new_game(request):
-    mastermind = Mastermind(4, 5)
-    serializedGame = serializers.serialize("json", [ mastermind.game, ])
 
+def new_game(request):
     result = {
         'res': 'OK',
-        'error': {},
-        'data': serializedGame,
+        'error': '',
+        'data': '',
     }
 
+    if request.method != 'POST':
+        result['res'] = 'ERROR'
+        result['error'] = 'This is a POST endpoint, you cannot invoke a GET'
+
+        return HttpResponse(json.dumps(result))
+
+    numberOfPegs = request.POST.get('numberOfPegs', '4')
+    numberOfChoices = request.POST.get('numberOfChoices', '5')
+
+    mastermind = Mastermind()
+    try:
+        mastermind.createNewGame(int(numberOfPegs), int(numberOfChoices))
+    except ValueError as err:
+        result['res'] = 'ERROR'
+        result['error'] = str(err)
+        
+        return HttpResponse(json.dumps(result))
+
+    result['data'] = gameToJson(mastermind.game, False)
+
     return HttpResponse(json.dumps(result))
+
 
 def game(request, game_id):
     mastermind = Mastermind(game_id)
@@ -31,3 +50,30 @@ def game(request, game_id):
     }
 
     return HttpResponse(json.dumps(result))
+
+
+def gameToJson(game, showPattern):
+    jsonGame = {
+        'id': game.id,
+        'started_at': game.started_at.strftime('%Y-%m-%d %H:%M:%S'),
+        'state': game.state,
+        'num_pegs': game.num_pegs,
+        'num_choices': game.num_choices,
+        'num_attempts': game.num_attempts,
+    }
+
+    attempts = []
+    for attempt in game.attempt_set.all():
+        attempt = {
+            'id': attempt.id,
+            'guess': patternToGuess(attempt.guess),
+            'black': attempt.correct_value_and_position,
+            'white': attempt.correct_value_not_position,
+        }
+        attempts.append(attempt)
+    jsonGame['attempts'] = attempts
+
+    if showPattern:
+        jsonGame['pattern'] = patternToGuess(game.pattern)
+
+    return jsonGame
